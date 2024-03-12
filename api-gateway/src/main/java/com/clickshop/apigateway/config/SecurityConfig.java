adf98e50b92537786bc2a5d745a3d2be3dbcaf6e
PrinceG24 <princegupta51020@gmail.com>
2024-03-11 17:33:19 -0700

    commit

---
+++ api-gateway/src/main/java/com/clickshop/apigateway/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.clickshop.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+         http.csrf().disable();
+//                .authorizeExchange((exchange) -> exchange.anyExchange()
+//                        .authenticated()
+                //);
+        return http.build();
+    }
+}
